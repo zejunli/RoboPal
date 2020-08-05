fde40db0deac2b9756fdaf98da06e68488f5ca03
zejun <li.zeju@husky.neu.edu>
2020-08-04 21:35:14 -0400

    Updates

---
+++ precache-manifest.0a57571b0d12f7e8f8fdd29319578098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa3160ebc9a51507b92ddcc126d26cd",
+    "url": "/RoboPal/index.html"
+  },
+  {
+    "revision": "6c25adb082624d390ff4",
+    "url": "/RoboPal/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6c7771b5cc04f2d22d5b",
+    "url": "/RoboPal/static/css/main.bff3653b.chunk.css"
+  },
+  {
+    "revision": "6c25adb082624d390ff4",
+    "url": "/RoboPal/static/js/2.3bc5f773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboPal/static/js/2.3bc5f773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7771b5cc04f2d22d5b",
+    "url": "/RoboPal/static/js/main.1e563532.chunk.js"
+  },
+  {
+    "revision": "98a030d4fcade53ff8cb",
+    "url": "/RoboPal/static/js/runtime-main.ed5b9e84.js"
+  }
+]);
