90f482dd7e9317c6ee26892a1ca65adfebd9d2e1
zejun <li.zeju@husky.neu.edu>
2020-08-04 21:55:31 -0400

    removed some verbose statement

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -18,11 +18,9 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		console.log("sending the request");
 		fetch("https://jsonplaceholder.typicode.com/users")
 		.then(response => {return response.json();})
 		.then(users => {this.setState({robots: users});})
-		console.log("sent the request");
 	}
 
 	// use arrow function(non-predefined functions) to prevent the mis-use of 'this'
